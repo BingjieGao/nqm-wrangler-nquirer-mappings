36f0d3a1cc2b9eb1c30f57c84321f99b08aaf092
Bingjie Gao <gilman.david04@gmail.com>
2017-02-21 20:18:54 +0000

    example databot.js created worked locally with databot-test.js

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 module.exports = (function() {
-	"use strict";
-	
-	const fs = require("fs");
+  "use strict";
+
+  const fs = require("fs");
   const request = require("request");
-	const csv = require("csvtojson");
-	
-	function databot(input, output, context) {
+  const csv = require("csvtojson");
+  const path = require("path");
+
+  function databot(input, output, context) {
+    // Load particular function file from "./lib" according to input mappingType
+    const mappingType = require(path.join(__dirname, path.join("lib", input.mappingType.serviceIdToLsoa)));
+
     // Databot can accept the source data as either a TDX resource ID refering to a raw file, or a URL.
-		if (!input.sourceResource && !input.sourceURL) {
-			output.error("invalid arguments - please supply either a source resource id or source url");
-			process.exit(1);
-		}
-		
+    if (!input.sourceResource && !input.sourceURL) {
+      output.error("invalid arguments - please supply either a source resource id or source url");
+      process.exit(1);
+    }
     // Load the source data as an HTTP stream.
-		let sourceStream;
+    let sourceStream;
     if (input.sourceResource) {
-      sourceStream = context.tdxApi.getRawFile(input.sourceResource);
+      output.debug("sourceResource existed, parsing csv files from TBX rawFile");
+      sourceStream = context.tdxApi.getRawFile(input.sourceResource.rawFile1);
     } else {
       sourceStream = request.get(input.sourceURL);
-    }    
-
+    }
+    output.setFileStorePath("./jsonFiles");
     // Generate the output file based on the databot instance id.
     const outputFilePath = output.getFileStorePath(`${context.instanceId}-output.json`);
-		const destStream = fs.createWriteStream(outputFilePath);
-				
-		output.debug("fetching source for %s, writing to %s", input.sourceResource || input.sourceURL, outputFilePath);
-		
+    const destStream = fs.createWriteStream(outputFilePath);
+        
+    output.debug("fetching source for %s, writing to %s", input.sourceResource || input.sourceURL, outputFilePath);
+    
     // Use the csv parser to read from the source, and emit an event for every JSON object.
     // The default csv parser options uses the first row of the csv to determine the JSON object properties.
     // csvtojson is very powerful - if you need more complex parsing (e.g. nested documents etc) it probably
@@ -35,7 +39,8 @@ module.exports = (function() {
       .fromStream(sourceStream)
       .on("json", (json) => {
         // A json object has been parsed => write it to the destination stream.
-        destStream.write(JSON.stringify(json));
+        mappingType(json, destStream);
+        // destStream.write(JSON.stringify(outputJson));
       })
       .on("done", () => {
         // Parser has finished.
@@ -50,8 +55,16 @@ module.exports = (function() {
         // An error occurred during parsing.
         output.abort("failure during csv parse: %s", err.message);
       });
-	}
-	
-	var input = require("nqm-databot-utils").input;
-	input.pipe(databot);
+  }
+  let input = null;
+  if (process.env.NODE_ENV === "test") {
+    // Requires nqm-databot-gpsgrab.json file for testing
+    input = require("./databot-test.js")(process.argv[2]);
+  } else {
+    // Load the nqm input module for receiving input from the process host.
+    input = require("nqm-databot-utils").input;
+  }
+
+// Read any data passed from the process host. Specify we're expecting JSON data.
+  input.pipe(databot);
 }());
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+DEBUG=databot-test NODE_ENV=test node index.js ./secret_config.json
